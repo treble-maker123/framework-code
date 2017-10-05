8124b50c81d63264353e265a9df0b448ca923390
Ziqiang Guan <wolfgang.guan56@gmail.com>
2017-10-05 13:29:55 -0400

    added async_await draft example

---
+++ async_javascript/async_await.js
@@ -0,0 +1,24 @@
+/**
+ * An example demonstrating the use of async/await in Javascript, which is introduced 
+ * with ECMAScript 8 (ES8)
+ * 
+ * To run this example, use Node v6.x
+ * 
+ * "node callback_hell.js"
+ */
+
+var db = require("./database");
+
+// must use Immediately-Invoked Function Expression (IIFE) to demonstrate this
+(async function() {
+    var status = [];
+    var data = await db.fetch_characters_promise(true);
+    data.forEach(async function(character) {
+        var res = await db.is_alive_promise(character.id, true);
+        console.log(`Number ${res.id} seems to be fine.`);
+        status.push({character: character.name, alive: res.isAlive});
+        if(status.length == data.length) {
+            console.log("Everyone seems to be fine!");
+        }
+    });
+})();
